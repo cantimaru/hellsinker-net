fdf2ac985b5c56262ca0ef4a91a3b714e1be804c
canti <masterman9100@gmail.com>
2026-08-02 09:57:02 -0400

    alt text + caption text & formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,28 @@
+<script>
+    let caption = 'The 5th edition release of Hellsinker.\'s disc displayed against the back of its case.'
+</script>
+
 <h1>...and Them called It "HellSinker"</h1>
-<hr>
+<hr />
 <h2>Hellsinker.net</h2>
-<p>Welcome to the unofficial website dedicated to Hellsinker, a doujin shoot-em-up feared by many and appreciated by few.</p>
-<p>With sparse information and no central landmark to be found on the internet, we've retaken the Hellsinker.net domain to share our captivation with the game and provide a museum of all things Hellsinker.</p>
+<p>
+  Welcome to the unofficial website dedicated to Hellsinker, a doujin
+  shoot-em-up feared by many and appreciated by few.
+</p>
+<p>
+  With sparse information and no central landmark to be found on the internet,
+  we've retaken the Hellsinker.net domain to share our captivation with the game
+  and provide a museum of all things Hellsinker.
+</p>
 <p>Please enjoy what we have to offer, and keep your dignity!</p>
-<hr>
-<img class="centeredimg" src="img/top_photo.webp">
+
+<hr />
+<figure>
+  <img
+    class="centeredimg"
+    src="img/top_photo.webp"
+    alt="{caption}"
+  />
+
+  <figcaption class="text-gray-500 text-xs text-center">{caption}</figcaption>
+</figure>

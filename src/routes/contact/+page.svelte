5507c96fbabdfa910baf917ce1c0752bef8190e2
canti <masterman9100@gmail.com>
2026-08-02 11:43:09 -0400

    credits & contributions page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,28 @@
-<h1>conmtactp age</h1>
+<h1>Credits, Contact, & Contributions</h1>
+<br>
+
+<h2>Site Admins</h2>
+<hr/>
+
+<ul>
+    <li>
+        <h3>canti</h3>
+        <p>fluxer: canti#7196</p>
+        <p>discord: cantimaru</p>
+    </li>
+    <li>
+        <h3>fatih</h3>
+        <p>unsure if you wanted me to link this back to you so i'll leave it blank for now</p>
+    </li>
+</ul>
+
+<h2>Credits</h2>
+<hr/>
+
+<h2>How to Contribute</h2>
+<p>Fluxer instance here https://fluxer.gg/E9yOeg8z
+
+Github page here https://github.com/cantimaru/hellsinker-net
+
+reach out give me a handjob whatever yada yada yada always looking for contributions yada yada yada</p>
+<hr/>

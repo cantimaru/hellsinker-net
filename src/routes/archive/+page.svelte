2f1d0598138de17d2ebdbac8b27f69be7a87f596
canti <masterman9100@gmail.com>
2026-08-02 09:57:39 -0400

    more page goal notes

--- src/routes/archive/+page.svelte
+++ src/routes/archive/+page.svelte
@@ -3,4 +3,10 @@ Not a scoreboard i just dont know what to call it yet
 
 I have pretty much everything ever stored on my homelab so I'm considering the route of letting people download the files directly from my machine because it would be dope Probably.
 
-This effectively concatenates the google drive with some other files I have laying around from various mega folders into one spot
+This effectively concatenates the google drive with some other files I have laying around from various mega folders into one spot, amounting in the following:
+
+Hellsinker 1st - 6th edition releases of the game
+Hellsinker FLAC OST & FanNamed OST (with combined tracks) + Unused Tracks
+Other tonnor music albums/collections: Long Long Journey, MGPK OST, Soundcloud collection
+Collection of fan-albums/compilations/etc: Without reStraint, Imitation, WORDCROCKERROR, Audience With Prayer, Shaftdiver OST, Puzzsinker OST
+Various doujinshi scans (I have some of my own not listed on the site that I'd like to scan and translate in addition) possibly with an in-browser reader & library? would be fun to figure out
--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -1,9 +1,12 @@
 <h1>links page</h1>
 I think I'm gonna split this page into a couple of sections:
 1. Official hellsinker sightings (steam page, some links to wayback machine stuff for those curious)
-https://store.steampowered.com/app/1067720/Hellsinker/
-https://sanagimaru.nobody.jp/
-
+Steam page - https://store.steampowered.com/app/1067720/Hellsinker/
+Official hellsinker distribution & news site - https://sanagimaru.nobody.jp/
+Hellsinker wikipedia - https://ja.wikipedia.org/wiki/Hellsinker.
+Tonnor interview - https://shmuplations.com/hellsinker/
+booth page - https://tonnor.booth.pm/items/872038
+merch/zazzle store - https://www.zazzle.com/store/tonnorstore/products
 2. Unofficial hellsinker sightings (other wikis, namely the tv tropes page and the korean wiki)
 Shmups wiki - https://shmups.wiki/library/Hellsinker
 Korean wiki - https://en.namu.wiki/w/HellSinker.
@@ -12,3 +15,7 @@ Tvtropes - https://tvtropes.org/pmwiki/pmwiki.php/VideoGame/Hellsinker
 Steam group - https://steamcommunity.com/groups/Hellsinker
 Scoreboard - http://hellsinker.royalflare.net/
 Art archive - https://drive.google.com/drive/folders/1awJ0YQg3GgbDbOnxa7KgooXAZvegr93M?usp=sharing
+
+4. dead pages
+old infoseek URL - http://wls.hp.infoseek.co.jp/
+old tonnor t-shirt store - http://www.ohyeah.jp/a/3354.html (designs are viewable here https://web.archive.org/web/20110917005241/http://www.ohyeah.jp/a/3354.html)

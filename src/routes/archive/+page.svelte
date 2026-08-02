edc7f76cddef4852da02484cfc55966d108ca9d6
canti <masterman9100@gmail.com>
2026-08-02 09:29:01 -0400

    archive page + purpose of links page

---
+++ src/routes/archive/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Archive of Achievement</h1>
+Not a scoreboard i just dont know what to call it yet
+
+I have pretty much everything ever stored on my homelab so I'm considering the route of letting people download the files directly from my machine because it would be dope Probably.
+
+This effectively concatenates the google drive with some other files I have laying around from various mega folders into one spot
--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -1,1 +1,14 @@
 <h1>links page</h1>
+I think I'm gonna split this page into a couple of sections:
+1. Official hellsinker sightings (steam page, some links to wayback machine stuff for those curious)
+https://store.steampowered.com/app/1067720/Hellsinker/
+https://sanagimaru.nobody.jp/
+
+2. Unofficial hellsinker sightings (other wikis, namely the tv tropes page and the korean wiki)
+Shmups wiki - https://shmups.wiki/library/Hellsinker
+Korean wiki - https://en.namu.wiki/w/HellSinker.
+Tvtropes - https://tvtropes.org/pmwiki/pmwiki.php/VideoGame/Hellsinker
+3. community made things (steam groups, loeaderboards, etc)
+Steam group - https://steamcommunity.com/groups/Hellsinker
+Scoreboard - http://hellsinker.royalflare.net/
+Art archive - https://drive.google.com/drive/folders/1awJ0YQg3GgbDbOnxa7KgooXAZvegr93M?usp=sharing

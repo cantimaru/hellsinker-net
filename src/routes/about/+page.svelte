01db0f91c49d642a96443f5c6203df4b4749eae4
Fatih <fatihhatesios@gmail.com>
2026-08-03 15:40:41 -0400

    about + media

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,19 +1,67 @@
+<script lang="ts">
+	let caption0 =
+		"Art of the first three playable EXECUTORs.";
+	let captionclip =
+	    "Clip of what a first attempt may look like.";
+	let captionmv =
+		'Video example of four "movesets".';
+	let caption428 =
+		'Clip of taking down all parts of a PRAYER.';
+</script>
+
 <h1>About Hellsinker</h1>
 <hr>
-<div class="paragraph"><p><span class="standout">Hellsinker</span> is a bullet hell shoot-em-up ("shmup") video game, which involves lots of shooting at enemies while dodging dense, complex barrages of bullets.</p>
-<p>While shmups are plentiful nowadays and have been iterated upon, Hellsinker has had a long time's worth of praise and infamy.
-It sets itself apart from others in the genre in many ways, with difficulty ironically NOT being one of those ways.
-Rather, the complexity of its mechanics and how much the game injects its setting directly into the gameplay, while mechanically and artistically inclined,
-is off-putting to those wanting to get on-boarded into the gameplay as soon as possible.
-Many people have not managed to stick with the game just from the main menu and game UI, for that matter - and within Hellsinker, progress is very much a
-personal growth, arguably moreso than in other games. It accepts few transferable skills, and the poetic chaos of the story leaves several layers to unravel at a time.</p>
-<p>To put it into perspective, the playable characters in Hellsinker ("EXECUTORs") are very, <i>very</i> different from each other.
-Functionally, each consists of a sizable moveset thanks to the game's 3-button system which employs button combinations and tap speeds to do different attacks.
-Because of this, choosing a character is like picking and committing to a <b>fighting game</b> character. Newcomers <i>will</i> be stuck in between
-learning the "system mechanics" of Hellsinker and learning all of the options, tactics and strategies specific to their chosen EXECUTOR.</p>
-<p>Despite that, practice will result in mastery of both, slowly resulting in less mistakes, optimization, and - where the game truly shines - aggressive, confident play.
-Trying to clear the game for the first time is a journey on its own that can take weeks, but once everything comes together, the game truly becomes as deep as the ocean.
-Levels become much more dynamic and hectic, reacting to your progress, while scoring becomes the encouraged, obvious goal - completely opposite of the defensive play
-that's required when starting out.</p>
-<p>If you're able to be patient and look past the superficial roadblocks, you'll be able to experience a carefully crafted and incredibly rewarding video game experience
-unlike anything that's out there.</p></div>
+<div class="paragraph">
+    <p><span class="standout">Hellsinker</span> is a bullet hell shoot-em-up ("shmup") video game, which involves lots of shooting at enemies while dodging dense, complex barrages of bullets. The story involves members of an organization who are launching their third attack on an ominous, great tower called the CARDINAL SHAFT, having to fight through onslaughts of "PRAYERs" birthed there. Going deeper, entering the trap, sinking into the hell that leads to HELLSINKER...</p>
+    
+    <figure>
+    	<img class="centeredimg" src="img/hs_trio.webp" alt={caption0} />
+    	<figcaption class="text-gray-500 text-xs text-center">{caption0}</figcaption>
+    </figure>
+    
+    <p>While shmups are plentiful nowadays and have been iterated upon, Hellsinker has had a long time's worth of praise and infamy.
+    It sets itself apart from others in the genre in many ways, with difficulty ironically NOT being one of those ways.
+    Rather, the complexity of its mechanics, and how much the game injects its setting directly into the gameplay - while mechanically and artistically inclined -
+    is off-putting to those wanting to get on-board with the gameplay as soon as possible.
+    Many couldn't stick with the game solely from the main menu and game UI, for that matter - and within Hellsinker, progress is very much a
+    personal growth, arguably moreso than in other games. It accepts few transferable skills, has a notably lengthly run time, and the poetic chaos of its storytelling leaves many layers to unravel at a time. But if anything, Hellsinker's greatest faults lies in its age as a program and the (purposeful or not) sub-optimal default settings that come shipped with it.</p>
+    
+    <figure>
+        <video autoplay muted loop playsinline class="centeredimg">
+            <source src="anim/hs_noob.webm" type="video/webm">
+        </video>
+        <figcaption class="text-gray-500 text-xs text-center">{captionclip}</figcaption>
+    </figure>
+    
+    <p>To put it into perspective, the playable characters in Hellsinker ("EXECUTORs") are very, <i>very</i> different from each other.
+    Functionally, each consists of a sizable moveset thanks to the game's 3-button system which employs button combinations and tap speeds to do different attacks.
+    Because of this, choosing a character is like picking and committing to a <b>fighting game</b> character. Newcomers <i>will</i> be stuck in between
+    learning the "system mechanics" of Hellsinker and learning all of the options, tactics and strategies specific to their chosen EXECUTOR.</p>
+    
+    <figure>
+        <video autoplay muted loop playsinline class="centeredimg">
+            <source src="anim/hs_movesets.webm" type="video/webm">
+        </video>
+        <figcaption class="text-gray-500 text-xs text-center">{captionmv}</figcaption>
+    </figure>
+    
+    <p>Despite that, practice will result in mastery of both, slowly resulting in less mistakes, optimization, and - where the game truly shines - aggressive, confident play.
+    Trying to clear the game for the first time is a journey on its own that can take weeks, but once everything comes together, the game truly becomes as deep as the ocean.
+    Levels become much more dynamic and hectic, reacting to your progress, while scoring becomes the encouraged, obvious goal - completely opposite of the defensive play
+    that's required when starting out.</p>
+    
+    <figure>
+        <video autoplay muted loop playsinline class="centeredimg">
+            <source src="anim/hs_u428.webm" type="video/webm">
+        </video>
+        <figcaption class="text-gray-500 text-xs text-center">{caption428}</figcaption>
+    </figure>
+    
+    <p>If you're able to be patient and look past the superficial roadblocks, you'll be able to experience a carefully crafted and incredibly rewarding video game experience
+    unlike anything that's out there. Past the immersive storytelling told through gameplay, a total of eight distinct shot types, three seperate scoring systems, high skill
+	ceilings and player expression, this unassuming doujin game going 20 years old can truly offer so much for all sorts of players.</p>
+	
+    <p>There's much more that can be gushed about Hellsinker - whether it's the one-man development team with its history, the music that perfectly matches the themes
+	of the game, the use of its graphics, and the incredibly obscure lore that has the potential to be a fountain of theories - this site was made as a tribute to all of it,
+	and hopefully to allow other fans out there to connect on what we love about this fun little shmup!</p>
+</div>

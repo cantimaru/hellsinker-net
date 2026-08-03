ff4b5161187fa463c61b57367fd524ab0ffb6730
Fatih <fatihhatesios@gmail.com>
2026-08-02 21:17:20 -0400

    about

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -1,1 +1,19 @@
-<h1>info page lawl</h1>
+<h1>About Hellsinker</h1>
+<hr>
+<div class="paragraph"><p><span class="standout">Hellsinker</span> is a bullet hell shoot-em-up ("shmup") video game, which involves lots of shooting at enemies while dodging dense, complex barrages of bullets.</p>
+<p>While shmups are plentiful nowadays and have been iterated upon, Hellsinker has had a long time's worth of praise and infamy.
+It sets itself apart from others in the genre in many ways, with difficulty ironically NOT being one of those ways.
+Rather, the complexity of its mechanics and how much the game injects its setting directly into the gameplay, while mechanically and artistically inclined,
+is off-putting to those wanting to get on-boarded into the gameplay as soon as possible.
+Many people have not managed to stick with the game just from the main menu and game UI, for that matter - and within Hellsinker, progress is very much a
+personal growth, arguably moreso than in other games. It accepts few transferable skills, and the poetic chaos of the story leaves several layers to unravel at a time.</p>
+<p>To put it into perspective, the playable characters in Hellsinker ("EXECUTORs") are very, <i>very</i> different from each other.
+Functionally, each consists of a sizable moveset thanks to the game's 3-button system which employs button combinations and tap speeds to do different attacks.
+Because of this, choosing a character is like picking and committing to a <b>fighting game</b> character. Newcomers <i>will</i> be stuck in between
+learning the "system mechanics" of Hellsinker and learning all of the options, tactics and strategies specific to their chosen EXECUTOR.</p>
+<p>Despite that, practice will result in mastery of both, slowly resulting in less mistakes, optimization, and - where the game truly shines - aggressive, confident play.
+Trying to clear the game for the first time is a journey on its own that can take weeks, but once everything comes together, the game truly becomes as deep as the ocean.
+Levels become much more dynamic and hectic, reacting to your progress, while scoring becomes the encouraged, obvious goal - completely opposite of the defensive play
+that's required when starting out.</p>
+<p>If you're able to be patient and look past the superficial roadblocks, you'll be able to experience a carefully crafted and incredibly rewarding video game experience
+unlike anything that's out there.</p></div>

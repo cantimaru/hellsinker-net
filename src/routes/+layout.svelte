336c11b839d6fd2dfd99763927941524cb1519f3
canti <masterman9100@gmail.com>
2026-08-03 12:07:39 -0400

    removing outdated comments

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
-<script lang='ts'>
+<script lang="ts">
 	import { page } from "$app/state";
 	import "$lib/assets/shared.css";
 	let { children } = $props();
 
-	import greenSquare from "$lib/assets/navIco/top.gif"; // reserved for home page link
-	import blueDiamond from "$lib/assets/navIco/c_blue.gif"; // for pages with no subpages
-	import blueArrow from "$lib/assets/navIco/p_blue.gif"; // for pages with subpages
+	import greenSquare from "$lib/assets/navIco/top.gif";
+	import blueDiamond from "$lib/assets/navIco/c_blue.gif";
+	import blueArrow from "$lib/assets/navIco/p_blue.gif";
 	import hsSeg from "$lib/assets/navIco/hs_seg.gif";
 	import hsGly from "$lib/assets/navIco/hs_glyph.gif";
 	import star from "$lib/assets/navIco/rz_star.gif";
@@ -13,25 +13,25 @@
 	import flash from "$lib/assets/navIco/hs_flash.gif";
 	import life from "$lib/assets/navIco/hs_life.gif";
 	import star0 from "$lib/assets/navIco/rz_star0.gif";
-	
+
 	import bannerTop from "$lib/assets/banner_top.png";
 	import logo from "$lib/assets/hellsinkernet.png"; //
-	
-	import "$lib/assets/font/fonts.css"
-	
+
+	import "$lib/assets/font/fonts.css";
+
 	const navItems = [
-        { href: "/", label: "Home", icon: greenSquare },
-        { href: "/about", label: "About", icon: blueDiamond },
-        { href: "/links", label: "Links", icon: blueArrow },
-        { href: "/story", label: "Game Info", icon: star0 },
-        { href: "/downloads", label: "Downloads", icon: down },
-        { href: "/tonnor", label: "Tonnor Ludography", icon: blueDiamond },
-        { href: "/archive", label: "Garland Archive", icon: hsSeg },
-        { href: "/fanworks", label: "Fanmade Works", icon: flash },
-        { href: "/media", label: "Media", icon: life },
-        { href: "#", label: "About HS.Net", icon: hsGly },
-        { href: "/contact", label: "Contact Us", icon: star, indent: true},
-    ];
+		{ href: "/", label: "Home", icon: greenSquare },
+		{ href: "/about", label: "About", icon: blueDiamond },
+		{ href: "/links", label: "Links", icon: blueArrow },
+		{ href: "/story", label: "Game Info", icon: star0 },
+		{ href: "/downloads", label: "Downloads", icon: down },
+		{ href: "/tonnor", label: "Tonnor Ludography", icon: blueDiamond },
+		{ href: "/archive", label: "Garland Archive", icon: hsSeg },
+		{ href: "/fanworks", label: "Fanmade Works", icon: flash },
+		{ href: "/media", label: "Media", icon: life },
+		{ href: "#", label: "About HS.Net", icon: hsGly },
+		{ href: "/contact", label: "Contact Us", icon: star, indent: true },
+	];
 </script>
 
 <div class="max-w-212 h-screen m-0 mx-auto">
@@ -45,13 +45,25 @@
 		<nav class="app-toc">
 			<ul class="navList">
 				{#each navItems as item}
-                    <li style="padding-left: {Number(item.indent ?? 0) * 1.25}rem;">
-                        <a href={item.href} aria-current={page.url.pathname === item.href}>
-                            <img src={item.icon} width="16" height="16" alt="■" class="object-contain"/>
-                            {item.label}
-                        </a>
-                    </li>
-                {/each}
+					<li
+						style="padding-left: {Number(item.indent ?? 0) *
+							1.25}rem;"
+					>
+						<a
+							href={item.href}
+							aria-current={page.url.pathname === item.href}
+						>
+							<img
+								src={item.icon}
+								width="16"
+								height="16"
+								alt="■"
+								class="object-contain"
+							/>
+							{item.label}
+						</a>
+					</li>
+				{/each}
 			</ul>
 		</nav>
 
@@ -61,11 +73,11 @@
 
 		<footer class="app-footer flex justify-between hs-main">
 			<p class="text-left">
-				<strong>Hellsinker.</strong><br>
+				<strong>Hellsinker.</strong><br />
 				© Ruminant's Whimper
 			</p>
 			<p class="text-right ml-auto">
-				canti * nautichor<br>
+				canti * nautichor<br />
 				2026 _ Present
 			</p>
 		</footer>

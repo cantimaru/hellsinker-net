c9eae4250bfef16ec6d461ace25d492a962b7fc8
canti <masterman9100@gmail.com>
2026-08-05 14:32:23 -0400

    doublespaced because im a worthless fucking moron

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,7 +28,7 @@
 		{ href: "/tonnor", label: "Tonnor Ludography", icon: blueDiamond },
 		{ href: "/archive", label: "Garland Archive", icon: hsSeg },
 		{ href: "/fanworks", label: "Fanmade Works", icon: flash },
-		{ label: "Art  Gallery", icon: life },
+		{ label: "Art Gallery", icon: life },
 		{ href: "/art/official", label: "Official", icon: life, indent: 1 },
 		{ href: "/art/official/other", label: "Other", icon: life, indent: 2 },
 		{ href: "/art/fanart", label: "Fanart", icon: life, indent: 1 },

995e2bc33214e39bcfb306f91de842ea3859e3e5
canti imaru <masterman9100@gmail.com>
2026-08-17 13:11:09 -0400

    scaffolding for cut-content page

---
+++ src/routes/cut-content/+page.svelte
@@ -0,0 +1,430 @@
+<p>this is a collection of shit that was cut from the game uhhhhhh fapping my boner you know the usual im just gonna dump some shit here for me to get back to later lelelele</p>
+
+<p>
+    どうして今になってこんな処に?
+……どうして
+君達には似合わない場所じゃないか
+君達が要らないものばかりの墓場だ
+かで良い処だけど
+中意味を失い行く僕等を
+ねえ　それを喜んで良いのかい？
+
+Why have you come to a place like this now?
+……Why
+Isn’t this a place that doesn’t suit you?
+A graveyard full of things you don't need
+It’s a nice place, though
+We're gradually losing our meaning
+Hey, is it really okay for you to be happy about that?
+</p>
+
+<p>the picture of the alternate route Seg1L (alt)</p>
+
+<div>
+<h3>int_w_ex.CTX (Intro with EXECUTOR)</h3>
+<p>
+    Oh, sorry.
+I hope you weren't waiting too long.
+<wait>
+<pg>
+Let's get started, then.
+Can't waste any more time.
+Just don't ask about anything classified, all right?
+<wait>
+<clear>
+The leader of the organization?
+<wait>
+<pg>
+Well, there is the popular theory...
+<wait>
+Answering that wouldn't be interesting.
+It's not like you came here to hear about that, is it?
+<wait>
+<clear>
+In short...
+Using the "inhuman" gives us opportunities.
+<wait>
+<pg>
+"Devils"?
+<wait>
+That's another way of putting it, I suppose.
+A little distasteful, but not entirely untrue.
+<wait>
+<pg>
+I'm surprised to hear someone as young as you use
+a term as old-fashioned as that.
+<wait>
+We wouldn't be where we are now without it, though.
+All right.
+<wait>
+<clear>
+To be honest, it looks to us like the rumors and theories
+about the unidentified creatures could be true, as well.
+<wait>
+<pg>
+For all we know, the true purpose of that muddy, lichen-stained
+"graveyard" might be to bind us all together.
+<wait>
+There may parts like that everywhere, but beyond a certain
+scope it's just too opaque to really...
+I guess I can't really talk about that. That's your job.
+<wait>
+<clear>
+Even "inhuman" used to be something of a euphemism -- a
+"chance" for the public to exercise "mercy" with words.
+That "you get to choose from twelve flavors of individuality" thing.
+<wait>
+<pg>
+It seems like a waste of time.
+Really, time isn't the issue -- it's space.
+<wait>
+<pg>
+Oh, clever.
+Yes, that's it... right, if you look at the "curse" the other way, it's freedom.
+Or like those light little pastries, with the flaky dough.
+<wait>
+It gives the outlook and leeway to decide to those who have nothing,
+changing even the most bottomless ecstasy into something shallow.
+Happiness is limited, regardless of past personality.
+Ah, yes. This is all hypothetical, of course.
+<wait>
+<clear>
+It's not that activity or ability is restricted at that time.
+At the top of the staircase from <<something useless>> to <<that>>,
+all that shallow freedom brings is a restriction of ability.
+Ironically enough, it's <<very well coordinated>>.
+<wait>
+<pg>
+The structure is the same as an oath from an ancient religion.
+And that process - madness, despair - is a weapon to destroy powerlessness.
+The self becomes a weapon.
+<wait>
+There is no regret, only the lingering ember of worldly desire.
+The result is a ghost, seeking <<the real thing>>...
+I remember someone putting it like that.
+<wait>
+<clear>
+Well, that sounds a little fishy. Good gossip material.
+It's not really an exaggeration, though.
+<wait>
+If a person finds out that something they have is a fake...
+Usually they'll go out and try to buy <<the real thing>>, right?
+<wait>
+<pg>
+I don't need to fabricate evidence.
+Is there influence, or isn't there?
+Or perhaps I should say: do we recognize it, or not?
+All that material does is act as a beacon.
+<wait>
+<clear>
+Hey, that's... not a very fair comparison.
+<wait>
+It's like using a sifter for the sake of shaking it.
+It's a chance to show something to someone who can't see.
+<wait>
+<pg>
+I'd like to say we wouldn't gain anything by lying here.
+I don't think anyone who fails to understand that will be useful.
+<wait>
+<pg>
+Well, now we're getting into trouble we don't need anywhere.
+Looking won't find anything good, or interesting.
+<wait>
+<clear>
+Let's change the subject.
+<wait>
+Throughout history, whenever we aim for something, 
+it's been a matter of keeping our eyes on the prize and 
+on our feet at the same time.
+The road to progress is full of potholes.
+<wait>
+<pg>
+A distant goal is effectively unreachable.
+Our idea has been to make the process of getting there itself the goal.
+Not that that's anything new, I suppose.
+It's a time-honored tradition, when undertaking long-term projects.
+<wait>
+<pg>
+Preparing that path is our job here in the interior.
+We need to have something to hand to the agents out there.
+<wait>
+<clear>
+Freedom?
+I thought I mentioned that earlier.
+<wait>
+It's nothing important, really.
+If it's an issue of "do we have it when we think we don't, or do we not
+have it when we think we do", then we can just say "we used to, and now
+it's gone" -- makes everything a lot simpler.
+If we don't worry about it we can advance, and if we do, we can't.
+<wait>
+<pg>
+Of course, it's not like everything always goes according to the plan.
+We like it that way. It makes things fun.
+<wait>
+You think that's strange? Well, we think it's worth it.
+You've never seen something like that? When everything collapses into chaos
+is when real heroes show what they can do, you know.
+<wait>
+<clear>
+I think we can go back to what we were talking about before, now.
+Where were we again..?
+<wait>
+<clear>
+Oh, of course. Yes, it's a form of genius, I think...
+We select for traits we think will work in combination with the technology.
+It's not exactly like we're pulling together whatever trash comes our way.
+<wait>
+<pg>
+ただそいつは、ただ無闇に切れすぎるだけの危なっかしい刃物や
+長すぎて取り回しが酷く不便な物差し、
+デカすぎて何を叩く為に有るのかすら解らない金槌みたいなもんさ
+原石だのと聴こえのいい呼び方で誤魔化すにゃ、少々禍々し過ぎる
+<wait>
+<pg>
+あんたなら知ってるだろう、古い時代の遠回しな揶揄の言葉
+作為ない現物を指して「面白いデザイン」だの「デッサンが狂ってる」だの
+まったくそういうものだと思って貰っていい　そう思うね
+<wait>
+<clear>
+で、そんなモンを便利と言う奴なんて世間にゃそう居やしない
+と言うより、そうそう居てもらっちゃたまらない訳だ
+<wait>
+<pg>
+朝食に使うベーコンを手斧でスライスする道理はないだろ？
+ま、爆薬で土を耕す馬鹿はいるがね
+そういう手合いと一緒にして貰いたくはないな
+<wait>
+<pg>
+豚をバラすには鉈、分銅を掴むにゃピンセット
+そして目の前には豚どもが延々と横たわってる、
+シンプルにそういう話さ
+<wait>
+<clear>
+で、二つ目の質問に繋がる訳だが……
+<wait>
+<pg>
+納得出来ない人間ならそれこそ産廃のように巷に転がってるさ
+察しが良さそうなんで先に釘を刺しとくが、何の産業かは聞いてくれるなよ
+<wait>
+そんな訳で一般枠の採用に関しては青田刈りをするまでもない
+ただ品質検査のラインがフル稼働でメンテの間も無くなるだけだ
+<wait>
+<clear>
+で、本命の《現場担当》の人員供給源についてだな
+<wait>
+<pg>
+多くの活動地域では福祉の名目という事になってるが
+それだけに大当たりが引かれた後のクジなのは間違いなくてな
+<wait>
+地味に通って、打ち漏らしに目星を付けるフリはしているが……
+勧められる狩場というのも生温い接待のようなもんでな
+断るも浸るもあまり良い顔をしない奴らが居るんで、
+正味な話、付き合いで格好だけって所だ
+<wait>
+<pg>
+それよりは名実共に《福祉の手が及んでいない》所まで足を伸ばす方が割が良い
+あんた等が書き立てるような不謹慎な物言いをするなら、ちょっとした宝探しさ
+<wait>
+<clear>
+そこを見てハイエナのように言われるのは甘んじて受けるが
+さっきも言ったようにリサイクル業であって生産業じゃないんでね……
+消費を督促する権利なんて無い
+あくまで提案って奴だよ、そう、提案
+<wait>
+<pg>
+誰に対する何の提案かって？
+<wait>
+外部に対する我々の有為性の提案さ
+その価値をどう捉えてどういう結果になるかまでは与り知らんが
+捉え方に相応しい結果になるってのは大体間違い無いんじゃないか
+少なくとも、俺達のそれなりの長さの歴史の中ではそうなってる
+<wait>
+<pg>
+深く関わらない限りは間違いなく便利、ってのがこの業種の常って奴さ
+それに、誰だって知り合いが多い程そういう所で揉め事は起こしたくないもんだ
+<wait>
+<clear>
+もし揉め事を確実に避けられる方法があるなら、間違い無くそれをやるだろう？
+なおかつ自分がやれる事であれば、他人の手を煩わせるまでもない
+<wait>
+<pg>
+まあこの話もこれくらいにしといてくれ
+どうにも脱線が過ぎて具合が良くない
+<wait>
+<clear>
+で、次はなんだった？
+<wait>
+<clear>
+重要な資産である遺物を独占してるんじゃないかって？
+<wait>
+重要と言うにも実際の処、扱えるものが居ないからウチに回ってくるんだろう
+正直手に負えないものが多すぎるんだよ
+ワケが解らんからワケの解らん奴らに任せろ、以上の何でもないだろ
+<wait>
+<pg>
+第一、そういったモンは押し並べてどこかしらイカれてた時代の産物だ
+現代の枠組みに還元できるような技術なんてそうそう有るもんじゃない
+<wait>
+<pg>
+既に代替の技術が発達してる時には今更で、入り込む余地なんか無えし
+復古される分野ってのは自給の当てがあるモンに限ると言っていい
+<wait>
+<clear>
+俺達の運用してる装備にしても《洗練されたスクラップ》の他に
+適切な表現は無いだろうな
+<wait>
+<pg>
+まったく、回りくどいったら無えさ
+自覚的かつ遠回りに、出来合いの物を再発明してるんだからな
+巷で見ない新鮮さっていう意味じゃあ、確かに《新しい》のかも知らんが
+<wait>
+<pg>
+まあそんなこんなで、せめて役得と解釈して貰いたいもんだ……
+っと、こりゃ広報の言い分そのまんまで面白くもないな
+<wait>
+<clear>
+ん？
+<wait>
+<pg>
+ああ昔はどうだったか知らないがね
+ウチを出汁に使おうなんて連中には不思議とお目に掛からないね
+<wait>
+ああ《不思議と》。　
+そうそう《不思議と》。
+<pg>
+……お、そろそろ締めかい？時間的に
+<wait>
+<clear>
+……何？
+<wait>
+<pg>
+おいおい、あれだけ話を聞いておきながら
+そんな解りきった事を尋ねるのかい、アンタ
+<wait>
+<pg>
+<pg>
+ここが何処か忘れてねえか？
+<wait>
+<pg>
+ここはゴミ棄て場だ
+そいつが土に還るまで待つ場所だぜ
+<wait>
+<end>
+</p>
+
+<div>
+    <h3>arsenal.ctx (weapons briefing of sorts?)</h3>
+    <p>
+        Well, fancy seeing you here.
+<wait>
+It's been a while, hasn't it?
+I would have prepared some snack for you if you'd called ahead...
+<wait>
+<pg>
+Mm? You just want that cookie?
+It's part of the decoration, or should I say...
+It's damp, and...oh my, you're not listening.
+<wait>
+<pg>
+I suppose you won't be needing any tea, then.
+Doesn't it feel a little wrong without it?
+<wait>
+<clear>
+Oh, is that what you were looking for?
+It's the long-awaited blade weapon.
+<wait>
+I think you'll find it best for hit-and-run tactics--
+a direct hit will end many battles, so it should come in handy.
+It's supposed to enter production at the second plant, if these tests go well.
+<wait>
+<pg>
+Well then...
+Well, I imagine it will be faster to see for yourself.
+Behold the destructive power of our research team's treasured creation!
+<wait>
+<clear>
+The recoil?
+<wait>
+The fixtures on your gauntlets should take care of most of it.
+You might feel a little numb, but I'm sure you'll get used to it.
+<wait>
+And it's much cuter than the cannons the Roadrunners
+and Crawlers get to lug around, don't you think?
+<wait>
+<pg>
+It's not exactly a physical weapon.
+<wait>
+Its power is drawn from your <<spirit>>, as is your ability to control it.
+Think of it as a razor sharper than any other: the important part is the
+initial velocity and angle of the stroke. The impact itself is only a matter
+of resisting the recoil.
+<wait>
+<pg>
+It's not a sword so much as a musical instrument.
+A flute which cuts the air itself.
+<wait>
+<clear>
+What? Keeping a spare?
+Doesn't that seem like a bit much?
+<wait>
+<pg>
+Huh? What do you mean?
+What? You want to use two at once!?
+<wait>
+Well...
+Theoretically speaking....
+It would greatly increase their destructive power, if you could somehow get
+their waves to amplify at the target area.
+<wait>
+<clear>
+But... how many people do you think could manage that?
+<wait>
+<pg>
+If your aim or coordination was just a little bit off, there's no telling
+what the recoil could do. It's too risky.
+<wait>
+I... suppose it does fit with the spirit of the weapon,
+but I really can't recommend it.
+Since when were you a gambler, anyway?
+<wait>
+<clear>
+It shouldn't matter since you're not human?
+You mean you actually think you'd be just fine with two?
+<wait>
+Well, your mileage is good enough that you probably 
+wouldn't have too much trouble carrying them around...
+but I think we should look at the <<yoyo>> first.
+<wait>
+You don't like the new model, right? I know.
+You're starting to sound like an old geezer!
+Or maybe like a spoiled princess in a fairy tale.
+<wait>
+<pg>
+So?
+Will you try it out?
+If you want to use both at once, do keep in mind 
+that we don't have any other working models. 
+I... don't think you'll end up breaking into pieces, but...
+<wait>
+<clear>
+Then again...
+I guess this is one of the perks of my job.
+<wait>
+<pg>
+What is? Well, you know...
+Normally you're the grumpy old master type, right?
+Right now, you look like a kid in a candy store.
+<wait>
+No, no, not your face. Your entire attitude.
+I guess maybe you can't tell yourself.
+<wait>
+<pg>
+Yeah, that.
+I can see more of you than other people, or something.
+<wait>
+<end>
+    </p>
+</div>
+
+</div>

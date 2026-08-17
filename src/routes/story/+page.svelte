e673cb81d3fd93496a648054356fc9640451355c
canti imaru <masterman9100@gmail.com>
2026-08-17 13:46:47 -0400

    lore page muthafucka9000 thank u Gaba i love u so fucking much bro.

--- src/routes/story/+page.svelte
+++ src/routes/story/+page.svelte
@@ -1,6 +1,99 @@
 <h1>Hellsinker Prestory</h1>
-<hr>
-<p>The majority of the contents on this page are paraphrased from the <a href="https://hellsinker-instruction.firebaseapp.com/instruction_e/">Hellsinker manual</a> included in the game files.</p> 
+<hr />
+<p>
+	The majority of the contents on this page are paraphrased from the <a
+		href="https://hellsinker-instruction.firebaseapp.com/instruction_e/"
+		>Hellsinker manual</a
+	> included in the game files.
+</p>
 <!-- remind me to migrate hosting of the manual to my server or a subdomain or something idont fucking know !!!!!!!!!!! -->
-<br>
-<p>A very, very long time ago, the world was plunged into turbulent chaos and rendered largely uninhabitable by a threat recorded as nothing but the "Final Enemy". To survive, humanity created the "Mechanism" - a perfect habitat, implied to be an unthinkably massive structure on an island, ushering in a golden era of peace known as the Brief Honeymoon. Over time, the Mechanism began to deteriorate and lose its rhythm, ultimately resulting in "The Collapse" - a disaster likely carried out by falling parts that caused constructive waves on the surface's waters. The Collapse left surviving humans to cluster around the multipurpose factories that once supported the Mechanism in this now-broken world. Over many years, humanity slowly reclaimed the wastelands, banded together into new nations, and began exploring the ancient ruins of old.</p>
+<br />
+<h2>BRIEF</h2>
+<p>
+	A very, very long time ago, the world was plunged into turbulent chaos and
+	rendered largely uninhabitable by a threat recorded as nothing but the
+	"Final Enemy". To survive, humanity created the "Mechanism" - a perfect
+	habitat, implied to be an unthinkably massive structure on an island,
+	ushering in a golden era of peace known as the Brief Honeymoon. Over time,
+	the Mechanism began to deteriorate and lose its rhythm, ultimately resulting
+	in "The Collapse" - a disaster likely carried out by falling parts that
+	caused constructive waves on the surface's waters. The Collapse left
+	surviving humans to cluster around the multipurpose factories that once
+	supported the Mechanism in this now-broken world. Over many years, humanity
+	slowly reclaimed the wastelands, banded together into new nations, and began
+	exploring the ancient ruins of old.
+</p>
+<h2>LONG</h2>
+<p>
+	At some point in the past, during a period known as the Brilliant Age, a
+	large-scale war devastated the world to the point that humanity teetered on
+	the brink of extinction. It's unknown what exactly caused the world to
+	descend into chaos, but humans have recorded the cause as the Final Enemy,
+	presumably some kind of sentient beings — or perhaps even the war itself,
+	the name humanity gave to the event.
+</p>
+
+<p>
+	People, in their struggle to survive, created a powerful machine called the
+	Mechanism, the HELLSINKER. The Mechanism was born from humanity's desire to
+	survive and prevail against the Final Enemy — a tool that would bring the
+	rays of light back. For years, the Mechanism proved to be a successful
+	creation; in fact, it saved humanity from its threat. Located on an
+	artificial island called Paradise, humanity built a massive tower around the
+	Mechanism known as the Cardinal Shaft, as if they wanted to protect the
+	HELLSINKER, or essentially turn it into a digital god by placing its core at
+	the top of the tower.
+</p>
+
+<p>
+	The years of freedom and peace from the thing or the beings capable of
+	annihilating the entire world were, in the past, known as the Brief
+	Honeymoon. But after an unknown number of years, the Honeymoon itself proved
+	to be merely the calm before the storm. Humanity created something so
+	powerful, since it had managed to save the world, that even they were unable
+	to control such a complex mechanism.
+</p>
+
+<p>
+	During those years of peace, another event that once again steered the world
+	toward destruction occurred when the Mechanism began to spiral out of
+	control: The Collapse. The Cardinal Shaft is massive enough to pierce the
+	earth and reach the sky, and the Mechanism's desire to gain self-control led
+	to an imbalance in nature, causing immense, unstoppable waves and tsunamis
+	that eventually engulfed the planet to the point where entire civilizations
+	were reduced to ruins and rubble. The world was shattered. People only
+	managed to survive around the factories that had previously served to
+	support the Mechanism.
+</p>
+
+<p>
+	Humanity was at a very delicate moment. The great disaster had almost
+	reverted the survivors to primitive times, when they couldn't even venture
+	far from their homes. They could have ceased to exist if they hadn't helped
+	each other, so that's exactly what they did. The survivors started living in
+	settlements, helping one another and exchanging materials with other
+	settlements to expand their territories and rebuild the world together.
+	Little by little, they were rising from the ashes of the Collapse.
+</p>
+
+<p>
+	Many years passed, and humanity didn't give up on its second chance at life.
+	Their continued territorial expansion and the creation of new nations
+	rekindled the flame within them, allowing them to feel the world anew — a
+	process akin to overcoming a trauma.
+</p>
+
+<p>
+	There's something poetic in the way people's horizon expanded as they
+	expanded their horizons.
+</p>
+
+<p>
+	The ghosts of the past haunted them once again when, during one of their
+	usual explorations, they came across the ancient ruins and artifacts of the
+	age of the Mechanism. But instead of naturally feeling unsettled, they
+	became mesmerized by the fragments of the past and the idea of living in the
+	Honeymoon era once again.
+</p>
+
+<p>They were fated to repeat it all over again.</p>

374c4aa5ae936c8a9cbea1b5c002612e0e858486
canti imaru <masterman9100@gmail.com>
2026-08-17 12:46:58 -0400

    please use wrapping text in your editor of choice

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,13 +7,10 @@
 <hr />
 <h2>Hellsinker.net</h2>
 <p>
-	&emsp;Welcome to the unofficial website dedicated to Hellsinker, a doujin
-	shoot-em-up feared by many and appreciated by few.
+	&emsp;Welcome to the unofficial website dedicated to Hellsinker, a doujin shoot-em-up feared by many and appreciated by few.
 </p>
 <p>
-	&emsp;With sparse information and no central landmark to be found on the internet,
-	we've retaken the Hellsinker.net domain to share our captivation with the
-	game and provide a museum of all things Hellsinker.
+	&emsp;With sparse information and no central landmark to be found on the internet, we've retaken the Hellsinker.net domain to share our captivation with the game and provide a museum of all things Hellsinker.
 </p>
 <p>&emsp;Please enjoy what we have to offer, and keep your dignity!</p>
 

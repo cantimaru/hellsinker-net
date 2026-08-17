bd4562bcf3ccaae54350139143fd00847d8d8c40
canti imaru <masterman9100@gmail.com>
2026-08-17 12:49:49 -0400

    some formatting shenanigans

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,29 +3,42 @@
 		"The 5th edition release of Hellsinker.'s disc displayed against the back of its case.";
 </script>
 
-<h1>...and Them called It "HellSinker"</h1>
-<hr />
-<h2>Hellsinker.net</h2>
-<p>
-	&emsp;Welcome to the unofficial website dedicated to Hellsinker, a doujin shoot-em-up feared by many and appreciated by few.
-</p>
-<p>
-	&emsp;With sparse information and no central landmark to be found on the internet, we've retaken the Hellsinker.net domain to share our captivation with the game and provide a museum of all things Hellsinker.
-</p>
-<p>&emsp;Please enjoy what we have to offer, and keep your dignity!</p>
+<div class="flex flex-col">
+	<h1>...and Them called It "HellSinker"</h1>
+	<hr />
+	<h2>Hellsinker.net</h2>
+	<p>
+		&emsp;Welcome to the unofficial website dedicated to Hellsinker, a
+		doujin shoot-em-up feared by many and appreciated by few.
+	</p>
+	<p>
+		&emsp;With sparse information and no central landmark to be found on the
+		internet, we've retaken the Hellsinker.net domain to share our
+		captivation with the game and provide a museum of all things Hellsinker.
+	</p>
+	<p>&emsp;Please enjoy what we have to offer, and keep your dignity!</p>
 
-<p class="signature hs-morse">— NAUTICHOR</p>
+	<p class="signature hs-morse">— NAUTICHOR</p>
 
-<figure>
-	<img class="centeredimg" src="img/top_photo.webp" alt={caption} />
-	<figcaption class="text-gray-500 text-xs text-center">{caption}</figcaption>
-</figure>
+	<figure>
+		<img class="centeredimg" src="img/top_photo.webp" alt={caption} />
+		<figcaption class="text-gray-500 text-xs text-center">
+			{caption}
+		</figcaption>
+	</figure>
 
-<p>
-	&emsp; Here on this site, you'll find an array of information pertaining to not only Hellsinker itself, but the surrounding community, development, background, and everything that comes together that makes Hellsinker such an important piece of work to us. This site is built upon a fascination, adoration, and true love for this game, from the ground up.
-</p>
-<p>
-	&emsp;We hope you enjoy your time spent here, and we look forward to expanding what we have in store for you all.
-</p>
+	<p>
+		&emsp; Here on this site, you'll find an array of information pertaining
+		to not only Hellsinker itself, but the surrounding community,
+		development, background, and everything that comes together that makes
+		Hellsinker such an important piece of work to us. This site is built
+		upon a fascination, adoration, and true love for this game, from the
+		ground up.
+	</p>
+	<p>
+		&emsp;We hope you enjoy your time spent here, and we look forward to
+		expanding what we have in store for you all.
+	</p>
 
-<p class="signature hs-morse">— CA.NTI</p>
+	<p class="signature hs-morse">— CA.NTI</p>
+</div>

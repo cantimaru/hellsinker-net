b066cd4c7ec0e103204ba24aab336dc90f45ccf1
canti imaru <masterman9100@gmail.com>
2026-08-17 13:36:58 -0400

    early credits tab I gotta add specific attributions later these are my fellas tho they lit

---
+++ src/routes/credits/+page.svelte
@@ -0,0 +1,20 @@
+<h2>Credits</h2>
+<ul>
+    <li>
+        <p>
+            GabaXIII: Provided a comprehensive compilation of the game's story.
+        </p>
+    </li>
+    <li>
+        <p>
+            friedbakedfish: sonion
+        </p>
+    </li>
+</ul>
+
+<h2>How to Contribute</h2>
+<p>Fluxer instance here https://fluxer.gg/E9yOeg8z
+
+Github page here https://github.com/cantimaru/hellsinker-net
+
+reach out give me a handjob whatever yada yada yada always looking for contributions yada yada yada</p>

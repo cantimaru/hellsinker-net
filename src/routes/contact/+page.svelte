c3c9e605e748303d51f86a790b44e920dabb0b3f
canti imaru <masterman9100@gmail.com>
2026-08-17 13:37:57 -0400

    truncated & shortened to simple reachouts + killed faith and replaced with nautichor

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,26 +1,16 @@
 <h1>Credits, Contact, & Contributions</h1>
-<br>
-
-<h2>Site Admins</h2>
 <hr/>
 
-<ul>
-    <li>
+<h2>Site Admins</h2>
+
+<ul class="flex justify-between text-center">
+    <li class="">
         <h3>canti</h3>
         <p>fluxer: canti#7196</p>
         <p>discord: cantimaru</p>
     </li>
     <li>
         <h3>fatih</h3>
-        <p>unsure if you wanted me to link this back to you so i'll leave it blank for now</p>
+        <p>BLANK!</p>
     </li>
 </ul>
-
-<h2>Credits</h2>
-
-<h2>How to Contribute</h2>
-<p>Fluxer instance here https://fluxer.gg/E9yOeg8z
-
-Github page here https://github.com/cantimaru/hellsinker-net
-
-reach out give me a handjob whatever yada yada yada always looking for contributions yada yada yada</p>

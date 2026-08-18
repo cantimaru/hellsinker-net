41c9316fd2a230703b11d8fc07cf698033ca5c27
canti imaru <masterman9100@gmail.com>
2026-08-18 16:03:55 -0400

    OKAY.

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,15 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
+	compilerOptions: {
+		runes: ({ filename }) =>
+			filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+	},
 	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: '404.html',
+			fallback: 'index.html',
 			precompress: false,
 			strict: true
 		})

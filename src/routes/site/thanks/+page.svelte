973ccdb38ff99d177a533a549ee367ae7783a3de
canti imaru <masterman9100@gmail.com>
2026-09-17 07:43:21 -0400

    updated & made some rearrangements to credits page

--- src/routes/site/thanks/+page.svelte
+++ src/routes/site/thanks/+page.svelte
@@ -2,20 +2,28 @@
 <ul>
 	<li>
 		<p>
-			GabaXIII: Provided a comprehensive compilation of the game's story.
+			GabaXIII: Provided the comprehensive compilation of the game's
+			story.
 		</p>
-	</li>
-	<li>
-		<p>friedbakedfish: sonion</p>
 	</li>
 </ul>
 
 <h2>Special Thanks</h2>
 <ul>
 	<li>
-		<p>
-			PeachyCatto: Media donation
-		</p>
+		<p>PeachyCatto: Media donation</p>
+	</li>
+	<li>
+		<p>friedbakedfish: Hellsinker scholar associate</p>
+	</li>
+	<li>
+		<p>Nia: Friend and webmaster of the <a href="https://radirgy.neocities.org/">Radirgy Database</a> Fansite</p>
+	</li>
+	<li>
+		<p>Amine: Hellsinker turbofan and provisoner of obscure TONNOR works</p>
+	</li>
+	<li>
+		<p>Beetle-Omiwatari: Datamining and excavator of a majority of Hellsinker's hidden content</p>
 	</li>
 </ul>
 

6aa83da28a63cb2351b16133707da1062fc0be24
canti imaru <masterman9100@gmail.com>
2026-09-21 14:15:26 -0400

    contributions changing + reordering and updated how to contribute section

--- src/routes/site/thanks/+page.svelte
+++ src/routes/site/thanks/+page.svelte
@@ -1,4 +1,5 @@
 <h1>Credits</h1>
+<h2>Direct Contributors</h2>
 <ul>
 	<li>
 		<p>
@@ -14,21 +15,21 @@
 		<p>PeachyCatto: Media donation</p>
 	</li>
 	<li>
-		<p>friedbakedfish: Hellsinker scholar associate</p>
-	</li>
-	<li>
-		<p>Nia: Friend and webmaster of the <a href="https://radirgy.neocities.org/">Radirgy Database</a> Fansite</p>
+		<p><a href="https://www.youtube.com/@beetle_omiwatari">Beetle-Omiwatari</a>: Datamining and excavator of a majority of Hellsinker's hidden content</p>
 	</li>
 	<li>
 		<p>Amine: Hellsinker turbofan and provisoner of obscure TONNOR works</p>
 	</li>
 	<li>
-		<p>Beetle-Omiwatari: Datamining and excavator of a majority of Hellsinker's hidden content</p>
+		<p>friedbakedfish: Hellsinker scholar associate</p>
+	</li>
+	<li>
+		<p>Nia: Friend, webmaster of the <a href="https://radirgy.neocities.org/">Radirgy Database</a> Fansite,</p> and inspiration for this site
 	</li>
 </ul>
 
 <h2>How to Contribute</h2>
 <ul>
-	<li>Fluxer instance here https://fluxer.gg/E9yOeg8z</li>
-	<li>Github page here https://github.com/cantimaru/hellsinker-net</li>
+	<li>Join the <a href="https://fluxer.gg/E9yOeg8z">Fluxer Community</a> for general Hellsinker discussion, site contribution, and more.</li>
+	<li>Open contribution GitHub source for the site located <a href="https://github.com/cantimaru/hellsinker-net">here</a>. If you're looking to contribute, join the Fluxer linked above to ask us directly.</li>
 </ul>

658e01d7e28e27a26b22af7e6ec7fe114037ff5f
canti imaru <masterman9100@gmail.com>
2026-09-21 15:44:29 -0400

    converted navbar to be general purpose component for future consistent stlye changes

---
+++ src/lib/horizontalNavbar.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { page } from "$app/state";
+	import "$lib/assets/shared.css";
+
+	let { navItems } = $props();
+</script>
+
+<nav>
+	<ul class="flex justify-between">
+		{#each navItems as item}
+			<li>
+				<a
+					href={item.href}
+					aria-current={page.url.pathname === item.href ? "page" : undefined}
+				>
+					{item.label}
+				</a>
+			</li>
+		{/each}
+	</ul>
+</nav>
--- src/routes/music/+layout.svelte
+++ src/routes/music/+layout.svelte
@@ -1,25 +1,19 @@
 <script>
 	import { page } from "$app/state";
 	import "$lib/assets/shared.css";
+    import HorizontalNavbar from "$lib/horizontalNavbar.svelte";
 	let { children } = $props();
-
+    
     const navItems = [
 		{ href: "/music/tracks", label: "Release"},
 		{ href: "/music/unused", label: "Unused"},
 		{ href: "/music/0.95", label: "0.95 Trial"},
 	];
+
 </script>
 
 <main class="app-content">
-    <nav>
-        <ul class="flex justify-between">
-            {#each navItems as item}
-            <li>
-                <a href={item.href} aria-current={page.url.pathname === item.href}>{item.label}</a>
-            </li>
-            {/each}
-        </ul>
-    </nav>
+    <HorizontalNavbar {navItems}/>
     
 	{@render children()}
 </main>
